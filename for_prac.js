80bde76e51f1731b466780596714abb7c1bda689
Jee Won Baek <mermaidstar103@gmail.com>
2024-06-10 10:20:13 +0900

    done

---
+++ for_prac.js
@@ -0,0 +1,23 @@
+
+const Jaurim = {
+  vocal : "김윤아",
+  guitar : "이선규",
+  base : "김진만",
+  drum : "구태훈" 
+}
+
+
+function searchName () {
+
+for (let i = 0; i < Jaurim.length; i++) {
+  // if (Jaurim.indexOf("김윤아") === toString)
+  // if (Jaurim[i]"김윤아") === toString)
+  if(Jaurim[i] === vocal && Jaurim[i] ==="김윤아")
+    {
+      return true;
+    }
+  }
+}
+
+let trueId = searchName(Jaurim);    
+console.log("true");
